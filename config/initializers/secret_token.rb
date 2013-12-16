671ef35aaa5089116651153732a4946992218da5
App Academy Student <student@appacademy.io>
2013-12-16 14:55:44 -0800

    Initial skeleton commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialThing::Application.config.secret_token = '6fefd12f0592fe68dee32f54d1bf31cac677821205abd1e8b79a8c55101a8769e6a5bf3755d07da893a2754cfd65d74483a7d1848be120b152d5804bb3de395a'
